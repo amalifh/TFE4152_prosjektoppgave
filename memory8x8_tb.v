61f6dc88a47e0382fb16b6c6e4e1e7ceed442775
Andrea Gladhaug <125492384+andrgla@users.noreply.github.com>
2024-11-20 18:22:22 +0100

    Update memory8x8_tb.v

--- memory8x8_tb.v
+++ memory8x8_tb.v
@@ -30,7 +30,7 @@ module memory8x8_tb;
     select = 1; rw = 1; address = 3'b000;
     data_in = 8'b00000000;
     #10;
-    rw = 0; // Write mode
+    rw = 0; 
     #10;
     #10;
 
@@ -38,13 +38,38 @@ module memory8x8_tb;
 
     // Write to Address 000 and Read Back
     rw = 1; // Write mode
-
     data_in = 8'b10101010;
-    #10;    // Allow write time
+    $display("Init Check: Addr=%b Data Out=%b (Expected: 00000000)", address, data_out);
+    #30;
     rw = 0; // Read mode
     #10;
+
+    $display("Write 10101010 at Addr 000: Data Out=%b (Expected: 10101010)", data_out);
+    #5;
+    $display("Write 10101010 at Addr 000: Data Out=%b (Expected: 10101010)", data_out);
+    #5;
+    $display("Write 10101010 at Addr 000: Data Out=%b (Expected: 10101010)", data_out);
+    #5;
+    $display("Write 10101010 at Addr 000: Data Out=%b (Expected: 10101010)", data_out);
+    #5;
     $display("Write 10101010 at Addr 000: Data Out=%b (Expected: 10101010)", data_out);
 
+    rw = 1; // Read mode
+    data_in = 8'b01010101;
+
+    #10;
+    rw = 0;
+    #10;
+    $display("Write 01010101 at Addr 000: Data Out=%b (Expected: 01010101)", data_out);
+    address = 3'b001;
+    rw = 1;
+    data_in = 8'b00010101;
+    #10
+    select = 1;
+    rw = 0;
+    #10
+    $display("Init Check: Addr=%b Data Out=%b (Expected: 00000000)", address, data_out);
+    #10 
     $finish;
     end
     
